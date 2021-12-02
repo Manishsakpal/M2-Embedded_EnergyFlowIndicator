56fdcc8b686a3939f9bf02994c2563836c54a623
Manishsakpal <86291115+Manishsakpal@users.noreply.github.com>
2021-12-02 13:29:29 +0530

    Update EnergyFlowIndicator.c

--- 3_Implementation/src/EnergyFlowIndicator.c
+++ 3_Implementation/src/EnergyFlowIndicator.c
@@ -1,29 +1,37 @@
+/**
+ * @file EnergyFlowIndicator.c
+ * @author Manish Sakpal (manish.sakpal@ltts.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-12-02
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #include <avr/io.h>
 #define F_CPU 1000000
 #include <util/delay.h>
 #include <stdlib.h>
-#include "Function.h"
-
-
+#include "Energyindicator.h"
 
 void send_a_command(unsigned char command)
 {
-    PORTD = command;
-    PORTB &= ~ (1<<registerselection);
-    PORTB |= 1<<enable;
-    _delay_ms(2);
-    PORTB &= ~1<<enable;
-    PORTD = 0;
+    PORTB = command;
+    PORTD &= ~ (1<<registerselection);
+    PORTD |= 1<<enable;
+    _delay_ms(20);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
 }
 
 void send_a_character(unsigned char character)
 {
-    PORTD = character;
-    PORTB |= 1<<registerselection;
-    PORTB |= 1<<enable;
-    _delay_ms(2);
-    PORTB &= ~1<<enable;
-    PORTD = 0;
+    PORTB = character;
+    PORTD |= 1<<registerselection;
+    PORTD |= 1<<enable;
+    _delay_ms(20);
+    PORTD &= ~1<<enable;
+    PORTB = 0;
 }
 void send_a_string(char *string_of_characters)
 {
